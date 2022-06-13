58718aa3d909534de96e9e6ed6cb890756b73126
Moulya K M <1rn19cs082.moulya@gmail.com>
2022-06-13 07:15:46 +0530

    Add files via upload

---
+++ Middlle_element_ll.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+ 
+
+
+
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+         int count=0;int i,j;
+         Node temp=head;
+         for(;temp!=null;temp=temp.next)
+         {
+             count++;
+         }
+         if(count%2==0)
+         {
+             int mid=(count/2)+1;
+             for( i=1,temp=head;i<mid;i++)
+             {
+                 temp=temp.next;
+             }
+             
+             return temp.data;
+         }
+         else{
+             int mid=(count/2);
+             for(j=0,temp=head;j<mid;j++)
+             {
+                 temp=temp.next;
+             }
+             
+             return temp.data;
+         }
+    }
+}
